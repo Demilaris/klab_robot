90f450661900376f46186932d56bd6b3fa8885c4
Lera <34194040+Demilaris@users.noreply.github.com>
2018-07-16 16:36:16 +0300

    Add files via upload

--- klab_robot_new.ino
+++ klab_robot_new.ino
@@ -1,46 +1,90 @@
-int left_motor_speed = 11; 
-int orange=10;
-int right_motor_speed=9;
-int yellow=6;
+int left_motor_speed = 10; //pwm1
+int left_dir=11;//dir1
+int right_motor_speed=8;//pwm2
+int right_dir=9;//dir2
 int rx,ry;
 void setup() {
 Serial.begin(9600);
 Serial1.begin(9600);
 pinMode(left_motor_speed, OUTPUT);
-pinMode(orange, OUTPUT);
+pinMode(left_dir, OUTPUT);
 pinMode(right_motor_speed, OUTPUT);
-pinMode(yellow, OUTPUT);
+pinMode(right_dir, OUTPUT);
 }
+
+/*
+ 0 - левый стик X  
+ 1 - левый стик Y
+ 2 - правый стик X
+ 3 - правый стик Y
+ 4 - А
+ 5 - В
+ 6 - X
+ 7 - Y
+ 8 - левый триггер
+ 9 - правый триггер
+ */
 byte buffer[10];
 void loop() {
-//  while(Serial1.available()>0){
    Serial1.readBytes(buffer,10);
-   
+      
    ry=-map((int)buffer[3],0,255,-255,255);
    rx=map((int)buffer[2],0,255,-255,255);
    if ((ry>=-2)&&(ry<=2)) ry=0;
    if((rx>=-2)&&(rx<=2)) rx=0;
-   if ((ry==0)&&(rx==0)) 
+  
+   if ((ry==0)&&(rx==0)) //стоим на месте
    {
    analogWrite(left_motor_speed,0);
    analogWrite(right_motor_speed,0);
    }
-   else
+   else if ( (ry>0)&&(rx==0))//движение вперед
+   { 
+   analogWrite(left_motor_speed,ry);
+   analogWrite(right_motor_speed,ry);
+   digitalWrite(left_dir,HIGH);
+   digitalWrite(right_dir,HIGH);
+   }
+   else if ( (ry<0)&&(rx==0))//движение назад
    { 
    analogWrite(left_motor_speed,abs(ry));
    analogWrite(right_motor_speed,abs(ry));
+   digitalWrite(left_dir,LOW);
+   digitalWrite(right_dir,LOW);
    }
-   if ((rx<0)&&(ry>0))
-   {analogWrite(right_motor_speed,constrain(abs((ry-rx)),0,255));
-   analogWrite(left_motor_speed,rx);
+   
+   if ((rx<0)&&(ry>0))//поворот налево
+   {analogWrite(right_motor_speed,constrain((ry+rx),0,255));
+   analogWrite(left_motor_speed,abs(rx));
+    digitalWrite(left_dir,HIGH);
+   digitalWrite(right_dir,HIGH);
    } 
 
-   if 
    
-   
-//     Serial.println(rx);
-//Serial.println(ry);
+  if ((rx>0)&&(ry>0))//поворот  направо
+   {analogWrite(right_motor_speed,abs(rx));
+   analogWrite(left_motor_speed,constrain((ry-rx),0,255));
+    digitalWrite(left_dir,HIGH);
+   digitalWrite(right_dir,HIGH);
+   } 
 
-//analogWrite(GREEN,constrain(255-i,128,255));
+if ((ry==0)&&(rx<0))//поворот на месте левым колесом
+{
+     analogWrite(left_motor_speed,abs(rx));
+   analogWrite(right_motor_speed,abs(rx));
+   digitalWrite(left_dir,LOW);
+   digitalWrite(right_dir,HIGH);
+}
+if ((ry==0)&&(rx>0))//поворот на месте правым колесом
+{
+     analogWrite(left_motor_speed,abs(rx));
+   analogWrite(right_motor_speed,abs(rx));
+   digitalWrite(left_dir,HIGH);
+   digitalWrite(right_dir,LOW);
+}
+
+//Serial.println(rx);
+////Serial.println(ry);
+
   }
-//}
+
